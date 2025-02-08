a3bc5c6a8f5d5b5bf757dc05424c499467c2c21e
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-08 11:14:06 +0330

    feat: add useClickOutside custom hook

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,18 @@
+import { RefObject, useEffect } from 'react';
+
+export const useClickOutside = <T extends HTMLElement>(
+  element: RefObject<T | null>,
+  callback: () => void
+) => {
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (element.current && !element.current?.contains(event.target as Node)) {
+        callback();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => document.removeEventListener('mousedown', handleClickOutside);
+  }, [element, callback]);
+};
