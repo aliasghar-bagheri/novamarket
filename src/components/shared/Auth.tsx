220c5894c858347c360967e77116c066257cf078
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-10 13:43:34 +0330

    feat: show user information in the header

--- src/components/shared/Auth.tsx
+++ src/components/shared/Auth.tsx
@@ -1,14 +1,26 @@
+'use client';
+
+import { useGetUser } from '@/hooks/auth/useGetUser';
 import { User } from 'lucide-react';
 import Link from 'next/link';
+import { CircleSkeleton } from '../ui/Skeleton';
 
 export default function Auth() {
+  const { data, isPending } = useGetUser();
+
   return (
-    <Link
-      href="/auth"
-      className="bg-secondary-0 p-3 rounded-3xl flex items-center gap-x-3 text-sm"
-    >
-      ورود و عضویت
-      <User className="bg-primary-500 rounded-full stroke-white w-8 h-8 p-1 shadow-md shadow-primary-500/50" />
-    </Link>
+    <>
+      {isPending ? (
+        <CircleSkeleton />
+      ) : (
+        <Link
+          href={data ? (data.user.role === 'ADMIN' ? '/dashboard' : '/profile') : '/auth'}
+          className="bg-secondary-0 p-3 rounded-3xl flex items-center gap-x-3 text-sm"
+        >
+          {!data && 'ورود و عضویت'}
+          <User className="bg-primary-500 rounded-full stroke-white w-8 h-8 p-1 shadow-md shadow-primary-500/50" />
+        </Link>
+      )}
+    </>
   );
 }
--- src/components/shared/layout/Cart/Cart.tsx
+++ src/components/shared/layout/Cart/Cart.tsx
@@ -1,16 +1,30 @@
+'use client';
+
 import Button from '@/components/ui/Button';
+import { useGetUser } from '@/hooks/auth/useGetUser';
 import { toPersianNumber } from '@/util/toPersianNumber';
 import { ShoppingBagIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Cart() {
+  const { data, isPending } = useGetUser();
+
   return (
-    <Button
-      variant="primary"
-      className="p-2 md:py-3 px-4"
-    >
-      <span className="hidden md:block">{toPersianNumber(0)}</span>
-      <p className="block md:hidden">سبد خرید</p>
-      <ShoppingBagIcon />
-    </Button>
+    <Link href="/cart">
+      <Button
+        variant="primary"
+        className="p-2 md:py-3 px-4"
+      >
+        <span className="hidden md:block">
+          {isPending ? (
+            <div className="w-4 h-5 rounded-xl animate-pulse bg-secondary-300"></div>
+          ) : (
+            toPersianNumber(data ? data.cart.productDetail.length : 0)
+          )}
+        </span>
+        <p className="block md:hidden">سبد خرید</p>
+        <ShoppingBagIcon />
+      </Button>
+    </Link>
   );
 }
