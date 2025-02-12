0d70950638305312470cddc44ee0a7d689c7cce6
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-12 12:11:39 +0330

    feat: add admin service

---
+++ src/services/admin.service.ts
@@ -0,0 +1,29 @@
+import { http } from '@/lib/axios/config';
+import { I_Payment, I_User } from '@/types';
+import { TServiceApiArguments } from '@/types/services';
+
+export async function getAllUsersApi(args?: TServiceApiArguments): Promise<I_User[]> {
+  try {
+    const { data } = await http.get(`/admin/user/list?${args?.queries}`, args?.options);
+
+    const { users } = data.data || {};
+
+    return users;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+export async function getAllPaymentsApi(args?: TServiceApiArguments): Promise<I_Payment[]> {
+  try {
+    const { data } = await http.get(`/admin/payment/list?${args?.options}`, args?.options);
+
+    const { payments } = data.data || {};
+
+    return payments;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
