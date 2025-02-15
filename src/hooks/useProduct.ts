180261ee6a1089f9312aaf80c0178dc5d94b274c
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-15 16:40:07 +0330

    feat: add custom hook to update product

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -1,9 +1,17 @@
-import { createProductApi } from '@/services/admin.service';
+import { createProductApi, updateProductApi } from '@/services/admin.service';
 import { useMutation } from '@tanstack/react-query';
 
 export const useCreateProduct = () => {
   const { mutateAsync, isPending } = useMutation({
     mutationFn: createProductApi,
+  });
+
+  return { mutateAsync, isPending };
+};
+
+export const useUpdateProduct = () => {
+  const { mutateAsync, isPending } = useMutation({
+    mutationFn: updateProductApi,
   });
 
   return { mutateAsync, isPending };
