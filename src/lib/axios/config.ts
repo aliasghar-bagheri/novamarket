548eb1f62f05636ad7d33a7b318bb1137c85520c
aliasghar bagheri <aliasgharbagheri.official@gmail.com>
2025-02-15 16:48:31 +0330

    feat: add axios interceptor to handle refresh token

--- src/lib/axios/config.ts
+++ src/lib/axios/config.ts
@@ -5,6 +5,28 @@ const app = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
+//  Manage refresh token when access token is not available
+app.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const orginalConfig = error.config;
+    if (error.response.status === 401 && !orginalConfig._retry) {
+      orginalConfig._retry = true;
+      try {
+        const { data } = await axios.get('/user/refresh-token', {
+          baseURL: process.env.NEXT_PUBLIC_API_URL,
+          withCredentials: true,
+        });
+
+        if (data) return app(orginalConfig);
+      } catch (error) {
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
 const http = {
   get: app.get,
   post: app.post,
